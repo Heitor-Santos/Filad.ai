bc3bfb2a5fbf9a0de3d2b731f2c4fccf0ebe7beb
Francisco Nascimento <fmln@cin.ufpe.br>
2021-08-08 00:22:05 -0300

    creating feedback route

--- backend/src/routes/estabelecimento-route.ts
+++ backend/src/routes/estabelecimento-route.ts
@@ -20,8 +20,45 @@ routes.get('/historico', async(req, res) => {
 
         return res.send(data);
     } catch (error) {
-        return res.status(400).send({error: 'Falou em retornar os estabelecimentos'});
+        return res.status(400).send({error: 'Falhou em retornar os estabelecimentos'});
     }
 });
+
+routes.post('/feedback', async(req, res) => {
+    try {
+        const {telegram_id, feedback} = req.body;
+
+        if(!telegram_id) {
+            return res.status(400).send({ error: 'Telegram_id não recebido'});
+        }
+
+        if(!feedback) {
+            return res.status(400).send({ error: 'Feedback não recebido'});
+        }
+
+        if(typeof(feedback.positivo) !== 'boolean' || typeof(feedback.descricao) !== 'string') {
+            return res.status(400).send({ error: 'Feedback não está no formato correto'});
+        }
+
+        let feedbackSaved: boolean = false;
+
+        atendimentos.forEach( (elem, index) => {
+            if(elem.telegram_id === telegram_id) {
+                atendimentos[index].feedback = { positivo: feedback.positivo, descricao: feedback.descricao};
+                feedbackSaved = true;
+            }
+        });
+
+        if(!feedbackSaved) {
+            return res.status(400).send({ error: 'Usuário não encontrado'});
+        }
+
+        return res.send({feedbackSaved});
+    } catch (error) {
+        return res.status(400).send({error: 'Falhou em salvar feedback'});
+    }
+});
+
+
 
 export default routes;
